f7688dd51193443b8cd1cd36f6b8e6831601a4bb
Camilla C <154556489+hicamilla@users.noreply.github.com>
2024-02-08 19:41:00 +0100

    search-engine-js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = searchInput.value;
+}
+
+
+
+
+let searchformElement = document.querySelector("#search-form");
+searchformElement.addEventListener("submit", handleSearchSubmit);
