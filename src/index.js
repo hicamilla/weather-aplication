e54ee61c622e943cf16ada652fc2f8b98cb9da2b
Camilla C <154556489+hicamilla@users.noreply.github.com>
2024-02-09 18:21:23 +0100

    forecast-js-input

--- src/index.js
+++ src/index.js
@@ -16,13 +16,12 @@ function refreshWeather(response) {
     iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temperature-icon"></img>`;
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = `${date.getHours()}:${date.getMinutes()}`;
-    temperatureElement.innerHTML = Math.round(temperature) + "°";
+    temperatureElement.innerHTML = Math.round(temperature);
     feels_likeElement.innerHTML = Math.round(response.data.temperature.feels_like) + "°";
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = response.data.temperature.humidity + "%";
     windElement.innerHTML = response.data.wind.speed + "km/h";
 }
-
 
 function searchCity(city) {
     let apiKey = "ec4af4682fo7f33ba0a6ate4046d3b06";
@@ -37,7 +36,35 @@ function handleSearchSubmit(event) {
     searchCity(searchInput.value);
 }
 
+function displayForecast() {
+    let forecast = document.querySelector("#forecast");
+    let days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
+    let forecastHtml = " ";
+
+    days.forEach(function (day) {
+        forecastHtml =
+          forecastHtml + `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15°</strong>
+          </div>
+          <div class="weather-forecast-temperature">9°</div>
+        </div>
+      </div>
+    `;
+    });
+
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHtml;
+ 
+}
+
 let searchformElement = document.querySelector("#search-form");
 searchformElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Oslo");
+displayForecast();
+
